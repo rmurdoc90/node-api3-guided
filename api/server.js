13a8c09eeea21b3b7c93da5f24624398ef159214
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-07-07 18:50:36 +0200

    wip

--- api/server.js
+++ api/server.js
@@ -8,6 +8,7 @@ server.use(express.json());
 
 server.use((req, res, next) => {
   console.log(`${req.method} request`)
+  req.foo = 'bar'
   next()
 }) // f takes req, res, next --> either call next  or send response to client
 
@@ -15,7 +16,7 @@ server.use('/api/hubs', hubsRouter);
 
 server.get('/', (req, res) => {
   res.send(`
-    <h2>Lambda Hubs API</h2>
+    <h2>Lambda Hubs API ${req.foo}</h2>
     <p>Welcome to the Lambda Hubs API</p>
   `);
 });
