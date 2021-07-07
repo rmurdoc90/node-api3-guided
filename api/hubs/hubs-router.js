0340b1c93b28efa9a4586e3f1762042bc3a975e9
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-07-07 19:21:20 +0200

    wip

--- api/hubs/hubs-router.js
+++ api/hubs/hubs-router.js
@@ -11,7 +11,10 @@ router.get('/', (req, res, next) => {
       res.status(200).json(hubs);
     })
     .catch(error => {
-
+      next({
+        custom: 'problem getting hubs',
+        message: error.message,
+      })
     });
 });
 
