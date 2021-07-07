901d602d830e25bcd3aaa951e3013660f00f2c86
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-07-07 18:37:54 +0200

    wip

--- api/server.js
+++ api/server.js
@@ -20,8 +20,10 @@ server.get('/', (req, res) => {
   `);
 });
 
-server.use('*', (req, res, next) => { // also can take next if needed
-  next({ status: 404, message: 'not found'})
+server.use('*', (req, res) => { // also can take next if needed
+  res.status(404).json({
+    message: 'not found, sorry!'
+  })
 })
 
 server.use((err, req, res, next) => { // eslint-disable-line
