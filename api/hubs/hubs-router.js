3d7796b9513df5e504a6a90ffea0d5a78af08871
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-07-07 19:20:32 +0200

    wip

--- api/hubs/hubs-router.js
+++ api/hubs/hubs-router.js
@@ -5,17 +5,13 @@ const Messages = require('../messages/messages-model.js');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
   Hubs.find(req.query)
     .then(hubs => {
       res.status(200).json(hubs);
     })
     .catch(error => {
-      // log error to server
-      console.log(error);
-      res.status(500).json({
-        message: 'Error retrieving the hubs',
-      });
+
     });
 });
 
