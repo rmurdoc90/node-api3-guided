1508dac5990791c1572402d36f2c41f572bb672c
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-07-07 18:31:37 +0200

    wip

--- api/server.js
+++ api/server.js
@@ -27,7 +27,10 @@ server.use('*', (req, res) => {
 })
 
 server.use((err, req, res, next) => {
-  
+  const status = err.status || 500
+  res.status(status).json({
+    message: err.message,
+  })
 })
 
 module.exports = server;
